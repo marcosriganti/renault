c9932a703e8988a4e8f5801cc4eb486e0acfa2d6
Marcos Javier Riganti <marcos.riganti@gmail.com>
2020-11-08 00:33:11 +0100

    adding Stats content

--- src/views/Stats/components/stats.tsx
+++ src/views/Stats/components/stats.tsx
@@ -15,17 +15,8 @@ const Stats = () => {
   const [userName, setUserName] = useState();
   const [users, setUsers] = useState([]);
   const history = useHistory();
-
-  const handleClick = (event: any) => {
-    event.preventDefault();
-
-    firebase
-      .auth()
-      .signOut()
-      .then((res) => {
-        history.push("/auth/login");
-      });
-  };
+  const [page, setPage] = useState(0);
+  const displayLength = 30;
 
   useEffect(() => {
     const db = firebase.firestore();
@@ -37,22 +28,36 @@ const Stats = () => {
         if (user) {
           // TODO set more data for the logged in user
           setUserName(user["username"]);
-          const userRef = db.collection("users").get();
+          const userRef = db.collection("users").limit(30).get();
           userRef
             .then(function (querySnapshot) {
               querySnapshot.forEach(function (doc) {
                 // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
+                const user = {
+                  id: doc.id,
+                  points: 0,
+                  level: 1,
+                  questions: 0,
+                  ...doc.data(),
+                };
+                let newUsers = users;
+                newUsers.push(user);
+                setUsers(newUsers);
               });
             })
             .catch(function (error) {
               console.log("Error getting documents: ", error);
             });
           // Get the List of users
+          console.log(users);
         }
       });
   }, []);
-
+  const tableData = users.slice(
+    page * displayLength,
+    page * displayLength + displayLength
+  );
+  console.log(users);
   return (
     <Container>
       <SharedHeader />
@@ -60,43 +65,48 @@ const Stats = () => {
         <FlexboxGrid justify="center" className="login-middle">
           <FlexboxGrid.Item colspan={10}>
             <h1>Estadisticas</h1>
-            <p className="text-center">
-              <div>
-                <Table
-                  virtualized
-                  height={400}
-                  data={[]}
-                  onRowClick={(data) => {
-                    console.log(data);
-                  }}
-                >
-                  <Column width={70} align="center" fixed>
-                    <HeaderCell>#</HeaderCell>
-                    <Cell dataKey="id" />
-                  </Column>
+            <div>
+              <Table
+                virtualized
+                height={400}
+                data={tableData}
+                onRowClick={(data) => {
+                  console.log(data);
+                }}
+              >
+                <Column width={70} align="center" fixed>
+                  <HeaderCell>#</HeaderCell>
+                  <Cell dataKey="id" />
+                </Column>
 
-                  <Column width={130}>
-                    <HeaderCell>Participante</HeaderCell>
-                    <Cell dataKey="firstName" />
-                  </Column>
+                <Column width={130}>
+                  <HeaderCell>Participante</HeaderCell>
+                  <Cell dataKey="username" />
+                </Column>
 
-                  <Column width={130}>
-                    <HeaderCell>Aciertos</HeaderCell>
-                    <Cell dataKey="lastName" />
-                  </Column>
+                <Column width={130}>
+                  <HeaderCell>Aciertos</HeaderCell>
+                  <Cell dataKey="questions" />
+                </Column>
 
-                  <Column width={200}>
-                    <HeaderCell>Nivel</HeaderCell>
-                    <Cell dataKey="city" />
-                  </Column>
+                <Column width={200}>
+                  <HeaderCell>Nivel</HeaderCell>
+                  <Cell dataKey="level" />
+                </Column>
 
-                  <Column width={200}>
-                    <HeaderCell>Puntos</HeaderCell>
-                    <Cell dataKey="street" />
-                  </Column>
-                </Table>
-              </div>
-            </p>
+                <Column width={200}>
+                  <HeaderCell>Puntos</HeaderCell>
+                  <Cell dataKey="points" />
+                </Column>
+              </Table>
+              <Pagination
+                activePage={page}
+                displayLength={displayLength}
+                total={users.length}
+                onChangePage={(dataKey) => setPage(dataKey)}
+                // onChangeLength={this.handleChangeLength}
+              />
+            </div>
           </FlexboxGrid.Item>
         </FlexboxGrid>
       </Content>
