b708df66110ba132c350c0e7d5efd4c1226743d7
Marcos Riganti <marcos.riganti@gmail.com>
2020-12-03 23:56:27 +0100

    improving mobile view for dashboard

--- src/views/Dashboard/components/dashboard.tsx
+++ src/views/Dashboard/components/dashboard.tsx
@@ -3,35 +3,13 @@ import { useHistory } from "react-router-dom";
 import firebase from "../../../firebase";
 import "firebase/firestore";
 
-import {
-  Content,
-  Container,
-  FlexboxGrid,
-  Icon,
-  Button,
-  Grid,
-  Row,
-  Col,
-} from "rsuite";
+import { Content, Container, Icon, Button, Grid, Row, Col } from "rsuite";
 import SharedFooter from "../../Shared/footer";
 import SharedHeader from "../../Shared/header";
 import DashboardImage from "../../../images/dashboard-img.png";
 
 const Dashboard = () => {
-  const [userName, setUserName] = useState("");
   const history = useHistory();
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        console.log("user", user);
-      } else {
-        // No user is signed in.
-      }
-    });
-    setUserName(firebase.auth().currentUser.displayName);
-  }, []);
 
   return (
     <Container>
@@ -50,57 +28,59 @@ const Dashboard = () => {
           </Row>
         </Grid>
 
-        <FlexboxGrid justify="center" className="body-text">
-          <FlexboxGrid.Item colspan={10}>
-            <p>
-              En el año del 65° aniversario de Fábrica Santa Isabel, queremos
-              invitarte a jugar y recorrer de manera divertida la historia de
-              nuestra Fabrica y de Renault.
-            </p>
-            <p>
-              Universo Renault es una plataforma online, donde podrás jugar y
-              poner a prueba tus recuerdos, tus conocimientos y convertirte en
-              un experto en la historia de nuestra Planta, mientras avanzás
-              nivel a nivel.
-            </p>
-            <p>
-              Este juego, además, fue diseñado, ilustrado y desarrollado por
-              Gota, un estudio de comunicación profesional, que cuenta con un
-              equipo creativo conformado por personas con discapacidad
-              intelectual.
-            </p>
-            <p>
-              A través de este juego, Renault continúa demostrando su compromiso
-              con los negocios inclusivos, generando trabajo genuino para
-              personas con discapacidad.
-            </p>
+        <Grid>
+          <Row style={{ textAlign: "left", padding: 20 }}>
+            <Col md={12} mdOffset={6}>
+              <p>
+                En el año del 65° aniversario de Fábrica Santa Isabel, queremos
+                invitarte a jugar y recorrer de manera divertida la historia de
+                nuestra Fabrica y de Renault.
+              </p>
+              <p>
+                Universo Renault es una plataforma online, donde podrás jugar y
+                poner a prueba tus recuerdos, tus conocimientos y convertirte en
+                un experto en la historia de nuestra Planta, mientras avanzás
+                nivel a nivel.
+              </p>
+              <p>
+                Este juego, además, fue diseñado, ilustrado y desarrollado por
+                Gota, un estudio de comunicación profesional, que cuenta con un
+                equipo creativo conformado por personas con discapacidad
+                intelectual.
+              </p>
+              <p>
+                A través de este juego, Renault continúa demostrando su
+                compromiso con los negocios inclusivos, generando trabajo
+                genuino para personas con discapacidad.
+              </p>
 
-            <p className="text-center">
-              <b>
-                ¡Encontrá los 65 objetos, reconocé su historia y disfrutá del
-                Universo Renault!
-              </b>
-            </p>
-            <div className="text-center">
-              <p>
-                <Icon icon="chevron-down" size={"2x"} />
+              <p className="text-center">
+                <b>
+                  ¡Encontrá los 65 objetos, reconocé su historia y disfrutá del
+                  Universo Renault!
+                </b>
               </p>
-              <p>
-                <Button color="yellow" onClick={() => history.push("/game")}>
-                  EMPEZAR A JUGAR
-                </Button>
-              </p>
-              <p>
-                <Button
-                  appearance="default"
-                  onClick={() => history.push("/stats")}
-                >
-                  ESTADISTICAS
-                </Button>
-              </p>
-            </div>
-          </FlexboxGrid.Item>
-        </FlexboxGrid>
+              <div className="text-center">
+                <p>
+                  <Icon icon="chevron-down" size={"2x"} />
+                </p>
+                <p>
+                  <Button color="yellow" onClick={() => history.push("/game")}>
+                    EMPEZAR A JUGAR
+                  </Button>
+                </p>
+                <p>
+                  <Button
+                    appearance="default"
+                    onClick={() => history.push("/stats")}
+                  >
+                    ESTADISTICAS
+                  </Button>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
       </Content>
       <SharedFooter />
     </Container>
